77728fd8b48fc290454e08bb14df596db81ddef7
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2022-07-15 11:26:41 -0400

    Removed commented out Docker run commands

--- ci/publish_build.sh
+++ ci/publish_build.sh
@@ -13,22 +13,18 @@ cd $APP_DIR
 git checkout $1
 
 # Initialize Terraform
-# docker run -v "$PWD:/app" -w "$INFRA_DIR" -e TF_VAR_bucket_name -e TF_VAR_domains -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY coloradodigitalservice/co-care-directory terraform init -backend-config="bucket=${TF_VAR_bucket_name}-terraform-state" -backend-config="dynamodb_table=${TF_VAR_bucket_name}-terraform-state"
 cd $INFRA_DIR
 terraform init -backend-config="bucket=${TF_VAR_bucket_name}-terraform-state" -backend-config="dynamodb_table=${TF_VAR_bucket_name}-terraform-state"
 
 # Apply the infrastructure
-# docker run -v "$PWD:/app" -w "$INFRA_DIR" -e TF_VAR_bucket_name -e TF_VAR_domains -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY coloradodigitalservice/co-care-directory terraform apply -auto-approve
 cd $INFRA_DIR
 terraform apply -auto-approve
 
 # Build the application
-# docker run -v "$PWD:/app" coloradodigitalservice/co-care-directory bash -c "npm install && npm run build"
 cd $APP_DIR
 npm install
 npm run build
 
 # Deploy the application
-# docker run -v "$PWD:/app" -e TF_VAR_bucket_name coloradodigitalservice/co-care-directory aws s3 sync build/. s3://$TF_VAR_bucket_name --delete
 cd $APP_DIR
 aws s3 sync build/. s3://$TF_VAR_bucket_name --delete
